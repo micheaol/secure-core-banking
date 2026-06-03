e925393bda47e2e342d4336e8ea2d57cb7d67417
micheaol <micheaol80@gmail.com>
2026-06-03 21:12:09 +0100

    Python list and Dictionary

---
+++ user_parser.py
@@ -0,0 +1,64 @@
+# API_KEY="Super-secret-key"
+
+# # List
+# users = [
+#     "Administrator",
+#     "admin",
+#     "guest",
+#     "test"
+# ]
+
+# # for user in users:
+# #     # print(f"Testing {user}")
+
+
+# # Dictionary
+# user = {
+#     "username":"Michael",
+#     "email":"test@gmail.com",
+#     "role":"admin"
+# }
+
+# user["is_active"] = True
+
+# # for key,value in user.items():
+# #     print(value)
+
+# # Turple: Once created, they cannot be changed.
+users = [
+    {
+        "username": "john",
+        "role": "user",
+        "active": True
+    },
+    {
+        "username": "admin",
+        "role": "administrator",
+        "active": True
+    },
+    {
+        "username": "guest",
+        "role": "user",
+        "active": False
+    },
+    {
+        "username": "backup_admin",
+        "role": "administrator",
+        "active": False
+    }
+]
+
+for user in users:
+    if user["active"]:
+        print(f"active_user {user["username"]}")
+
+for user in users:
+    if user["role"] == "administrator":
+        print(user["username"])
+
+active_user = 0
+for user in users:
+    if user["active"]:
+        active_user +=1
+
+print(active_user)
