97c4f7878c098a618cb399b6fbeba94c53c21d4f
micheaol <micheaol80@gmail.com>
2026-06-03 21:56:04 +0100

    Python list and Dictionary

--- user_parser.py
+++ user_parser.py
@@ -1,64 +1,82 @@
-# API_KEY="Super-secret-key"
+# # API_KEY="Super-secret-key"
 
-# # List
+# # # List
+# # users = [
+# #     "Administrator",
+# #     "admin",
+# #     "guest",
+# #     "test"
+# # ]
+
+# # # for user in users:
+# # #     # print(f"Testing {user}")
+
+
+# # # Dictionary
+# # user = {
+# #     "username":"Michael",
+# #     "email":"test@gmail.com",
+# #     "role":"admin"
+# # }
+
+# # user["is_active"] = True
+
+# # # for key,value in user.items():
+# # #     print(value)
+
+# # # Turple: Once created, they cannot be changed.
 # users = [
-#     "Administrator",
-#     "admin",
-#     "guest",
-#     "test"
+#     {
+#         "username": "john",
+#         "role": "user",
+#         "active": True
+#     },
+#     {
+#         "username": "admin",
+#         "role": "administrator",
+#         "active": True
+#     },
+#     {
+#         "username": "guest",
+#         "role": "user",
+#         "active": False
+#     },
+#     {
+#         "username": "backup_admin",
+#         "role": "administrator",
+#         "active": False
+#     }
 # ]
 
-# # for user in users:
-# #     # print(f"Testing {user}")
+# for user in users:
+#     if user["active"]:
+#         print(f"active_user {user["username"]}")
 
+# for user in users:
+#     if user["role"] == "administrator":
+#         print(user["username"])
 
-# # Dictionary
-# user = {
-#     "username":"Michael",
-#     "email":"test@gmail.com",
-#     "role":"admin"
-# }
+# active_user = 0
+# for user in users:
+#     if user["active"]:
+#         active_user +=1
 
-# user["is_active"] = True
+# print(active_user)
 
-# # for key,value in user.items():
-# #     print(value)
+api_response = {
+    "users": [
+        {
+            "username": "john",
+            "role": "user"
+        },
+        {
+            "username": "admin",
+            "role": "administrator"
+        }
+    ]
+}
 
-# # Turple: Once created, they cannot be changed.
-users = [
-    {
-        "username": "john",
-        "role": "user",
-        "active": True
-    },
-    {
-        "username": "admin",
-        "role": "administrator",
-        "active": True
-    },
-    {
-        "username": "guest",
-        "role": "user",
-        "active": False
-    },
-    {
-        "username": "backup_admin",
-        "role": "administrator",
-        "active": False
-    }
-]
-
-for user in users:
-    if user["active"]:
-        print(f"active_user {user["username"]}")
-
-for user in users:
-    if user["role"] == "administrator":
-        print(user["username"])
-
-active_user = 0
-for user in users:
-    if user["active"]:
-        active_user +=1
-
-print(active_user)
+for key,value in api_response.items():
+    for user in value:
+        if user["role"] == "administrator":
+            print(f"admin_user: {user["username"]}")
